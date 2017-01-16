f3768897b200f6032a2c3fa27f09f26c4a4b3a51
Anna Sedlar <annasedlar@gmail.com>
2017-01-16 11:59:54 -0500

    display movie posters

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
+//Dependencies, someone else made this stuff --React stuff
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+//Custom modules/components
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Poster from './poster.js';
+//Custom CSS
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            moviePosters: []
+        }
+    }   
+
+    componentDidMount() {
+        var url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5';
+        $.getJSON(url, (movieData) =>{
+            console.log(movieData);
+            this.setState({
+                moviePosters: movieData.results
+            })
+        })
+    }
+
+    //load up the moviePosters array with poster components
+	render() {
+        var postersArray = [];
+        this.state.moviePosters.map((poster, index) => {
+            postersArray.push(<Poster poster={poster} key={index} />)
+        });
+		return (
+		<div className="container">
+            <div className="row">Movie Posters</div>
+			<div className="row" >
+                <h1>{postersArray}</h1>
+			</div>
+		</div>
+		);
+	}
 }
 
 export default App;
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+
+
+//Dependencies (someone else made these) -- React Stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
+
+//Custom Modules
 import App from './App';
+
+//Custom CSS
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render((
+  <Router history={hashHistory} >
+  	<Route path="/" component={App} />
+  </Router>),
+
   document.getElementById('root')
 );
